432a141f16f6460a3ed226595694abe3d5da79f4
aspz <aspz@bk.ru>
2015-12-10 12:51:04 +0300

    new settings

--- js.js
+++ js.js
@@ -1,23 +1,23 @@
 ﻿$('form').on('submit', function(e) {
-	e.preventDefault();
+  e.preventDefault();
   var valid = /^(ftp|http|https):\/\/[^ "]+$/.test('' + area.value);
   if ( valid ) {
-		var newImg = document.createElement('img');
-		$(newImg).attr('src', area.value);
-		var newLi = document.createElement('li');
-		newLi.appendChild(newImg);
-		res.appendChild(newLi);
-  	if ( comment.value ) {
-			var newCom = document.createElement('li');
-			newCom.innerHTML = comment.value;
-			res.appendChild(newCom);
-			$(newImg).click(function() {
-			  $(newCom).toggle();
-			});
- 	  }
+    var newImg = document.createElement('img');
+    $(newImg).attr('src', area.value);
+    var newLi = document.createElement('li');
+    newLi.appendChild(newImg);
+    res.appendChild(newLi);
+    if ( comment.value ) {
+      var newCom = document.createElement('li');
+      newCom.innerHTML = comment.value;
+      res.appendChild(newCom);
+      $(newImg).click(function() {
+        $(newCom).toggle();
+      });
+    }
   }
 else alert('Ошибка! Введите ссылку на картинку');
 });
 $('#knopka').on('click', function() {
-	$('li').remove();
+  $('li').remove();
 })
